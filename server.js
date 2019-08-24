2b62fa05f3f8354dec8452adc078c813ee80ec00
Huey <leejinhuey@gmail.com>
2019-08-24 10:59:09 +0800

    updated dependencies

--- server.js
+++ server.js
@@ -1,42 +1,44 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== `production`) {
+  require(`dotenv`).config()
+}
 
-const fs = require("fs");
-const Koa = require("koa");
-const bodyparser = require("koa-bodyparser");
-const session = require("koa-session");
-const mount = require("koa-mount");
-const { jsonify, logger, timer } = require("./middleware");
-const URL = require("url");
-const redis = require("redis");
-const { promisify } = require("util");
-const Raven = require("raven");
-const router = require("./router");
-const { app: UCLAPI } = require("./uclapi");
-const { app: notifications } = require("./notifications");
+const fs = require(`fs`)
+const Koa = require(`koa`)
+const bodyparser = require(`koa-bodyparser`)
+const session = require(`koa-session`)
+const mount = require(`koa-mount`)
+const { jsonify, logger, timer } = require(`./middleware`)
+const URL = require(`url`)
+const redis = require(`redis`)
+const { promisify } = require(`util`)
+const Raven = require(`raven`)
+const router = require(`./router`)
+const { app: UCLAPI } = require(`./uclapi`)
+const { app: notifications } = require(`./notifications`)
 
-const { version } = JSON.parse(fs.readFileSync("./package.json"));
+const { version } = JSON.parse(fs.readFileSync(`./package.json`))
 
-const sentryDsnUrl = process.env.SENTRY_DSN;
+const sentryDsnUrl = process.env.SENTRY_DSN
 
 if (sentryDsnUrl === undefined) {
-  console.error("Sentry DSN not provided!");
-  Raven.config().install();
+  console.error(`Sentry DSN not provided!`)
+  Raven.config().install()
 } else {
-  Raven.config(process.env.SENTRY_DSN).install();
+  Raven.config(process.env.SENTRY_DSN).install()
 }
 
-const connectionString = process.env.REDIS_URL;
+const connectionString = process.env.REDIS_URL
 
 if (connectionString === undefined) {
-  console.error("Please set the REDIS_URL environment variable");
-  process.exit(1);
+  console.error(`Please set the REDIS_URL environment variable`)
+  process.exit(1)
 }
 
-const app = new Koa();
+const app = new Koa()
 
-app.context.version = version;
+app.context.version = version
 
-console.log(`Running server version ${version}`);
+console.log(`Running server version ${version}`)
 
 if (
   !process.env.UCLAPI_CLIENT_ID ||
@@ -44,55 +46,55 @@ if (
   !process.env.UCLAPI_TOKEN
 ) {
   console.error(
-    "Error! You have not set the UCLAPI_CLIENT_ID, UCLAPI_TOKEN, or UCLAPI_CLIENT_SECRET environment variables.",
-  );
-  console.log("Please set them to run this app.");
-  process.abort();
+    `Error! You have not set the UCLAPI_CLIENT_ID, UCLAPI_TOKEN, or UCLAPI_CLIENT_SECRET environment variables.`,
+  )
+  console.log(`Please set them to run this app.`)
+  process.abort()
 }
 
 if (!process.env.SECRET) {
   console.warn(
-    "Warning: You have not set the SECRET environment variable. This is not secure and definitely not recommended.",
-  );
+    `Warning: You have not set the SECRET environment variable. This is not secure and definitely not recommended.`,
+  )
 }
 
 if (!process.env.NOTIFICATIONS_URL) {
   console.warn(
-    "Warning: You have not set the NOTIFICATION_URL environment variable. This means that notification actions will be disabled.",
-  );
+    `Warning: You have not set the NOTIFICATION_URL environment variable. This means that notification actions will be disabled.`,
+  )
 }
 
-app.keys = [process.env.SECRET || "secret"];
+app.keys = [process.env.SECRET || `secret`]
 
-if (connectionString.startsWith("rediss://")) {
+if (connectionString.startsWith(`rediss://`)) {
   app.context.redisClient = redis.createClient(connectionString, {
     tls: { servername: new URL(connectionString).hostname },
-  });
+  })
 } else {
-  app.context.redisClient = redis.createClient(connectionString);
+  app.context.redisClient = redis.createClient(connectionString)
 }
 
 app.context.redisGet = promisify(app.context.redisClient.get).bind(
   app.context.redisClient,
-);
+)
 app.context.redisSetex = promisify(app.context.redisClient.setex).bind(
   app.context.redisClient,
-);
+)
 app.context.redisSet = promisify(app.context.redisClient.set).bind(
   app.context.redisClient,
-);
+)
 
-app.use(session({}, app));
+app.use(session({}, app))
 
-app.use(bodyparser());
-app.use(timer);
-app.use(logger);
-app.use(jsonify);
+app.use(bodyparser())
+app.use(timer)
+app.use(logger)
+app.use(jsonify)
 // import and use the UCL API router.
-app.use(mount("/notifications", notifications));
-app.use(mount(UCLAPI));
-app.use(mount(router));
+app.use(mount(`/notifications`, notifications))
+app.use(mount(UCLAPI))
+app.use(mount(router))
 
 Raven.context(() => {
-  app.listen(process.env.PORT || 3000);
-});
+  app.listen(process.env.PORT || 3000)
+})
