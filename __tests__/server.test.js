8acea5f85186fc175f455c9929741aab23f97146
Huey <leejinhuey@gmail.com>
2019-09-04 12:27:03 +0800

    shutdown server after tests run

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -4,19 +4,30 @@
  */
 require(`dotenv`).config()
 const request = require(`supertest`)
-const server = require(`../server`)
+const app = require(`../server`)
 
 const indexRoutes = require(`../constants/indexRoutes`)
 
 describe(`test server`, () => {
+
+  let server
+
+  beforeAll(() => {
+    server = app.listen()
+  })
+
+  afterAll(() => {
+    server.close()
+  })
+
   it(`responds with route indexes at /`, async () => {
-    return await request(server.listen())
+    return await request(server)
       .get(`/`)
       .expect(200, { content: indexRoutes, error: `` })
   })
 
   it(`should pong when we ping`, async () => {
-    return await request(server.listen())
+    return await request(server)
       .get(`/ping`)
       .expect(200, { content: `pong!`, error: `` })
   })
